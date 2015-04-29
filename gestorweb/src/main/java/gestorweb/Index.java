09a92c7887c16334618f8a6b2fa6e4aff0437246
felipe <felipe@nbfelipe>
2015-04-29 14:38:13 -0300

    css

---
+++ gestorweb/src/main/java/gestorweb/Index.java
@@ -0,0 +1,23 @@
+package gestorweb;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean
+public class Index {
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Bean executado");
+
+	}
+
+	public String getMessage() {
+
+		return "Hello world!!!";
+
+	}
+
+}
